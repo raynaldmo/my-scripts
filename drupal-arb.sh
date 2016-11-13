e8be51f2cda5f751f9d6c06a28fbbed3677c4db2
Raynald Mompoint <raynaldmo@gmail.com>
2016-11-13 15:54:28 -0800

    Update to use appropriate drush version

--- drupal-arb.sh
+++ drupal-arb.sh
@@ -7,30 +7,50 @@
 # don't execute any commands just echo
 # set -xvn
 
-set -xv
+# echo and execute commands
+# set -xv
 
-COMPOSER=$HOME/.config/composer
-DRUSH=${COMPOSER}/vendor/drush/drush/drush
 GZIP=/usr/bin/gzip
 MV=/bin/mv
 LN=/bin/ln
 LS=/bin/ls
 RM=/bin/rm
 
-if [ $# -ne 2 ]
-then
-	echo "Usage : drupal-arb.sh <site_name> <backup_dir>"
-	echo "Usage : drupal-arb.sh example.com /home/sites/example.com/Development/backups"
+function usage () {
+    echo "Usage : drupal-arb.sh <drupal_version> <site_name> <backup_dir>"
+	echo "Usage : drupal-arb.sh 8 example.com /home/sites/example.com/Development/backups"
 	exit 1
+}
+
+if [ $# -ne 3 ]
+then
+	usage
 fi
 
-# to do
-# check if composer and drush is installed
-# check if command is run from drupal root
-# check if backup directory exists
+drupal_version=$1
+site_name=$2
+backup_dir=$3
 
-site_name=$1
-backup_dir=$2
+
+# check if appropriate drush is installed
+case "$drupal_version" in
+    8) DRUSH=$HOME/drush8/vendor/bin/drush ;;
+    7) DRUSH=$HOME/drush7/vendor/bin/drush ;;
+    6) DRUSH=$HOME/drush6/vendor/bin/drush ;;
+    *) usage ;;
+esac
+
+echo "$($DRUSH --version)"
+
+# check if backup directory exists
+if [ ! -d $backup_dir ]
+then
+    echo "Backup directory $backup_dir doesn't exist. Please specify a valid backup directory."
+    exit 2;
+fi
+
+# todo
+# check if command is run from drupal root
 
 echo 'creating archive...'
 ${DRUSH} archive-backup --destination=${backup_dir}/${site_name}-backup.tar \
