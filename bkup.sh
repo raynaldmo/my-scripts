cccb6317a8e1bcc1df590a33f9933c7b188c3ce5
raynaldmo <raynaldmo@gmail.com>
2017-05-28 23:37:24 -0700

    Simple backup script
    * Backs up home and /var/www directories on my CentOS 7 dev machine
      to external hard drive

---
+++ bkup.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# simple script to back up files
+
+home_dir=/home/raynaldmo
+www_dir=/var/www
+
+backup_loc=/run/media/raynaldmo/Seagate500
+
+home_backup=bkup-raynaldmo-home.tgz
+var_www_backup=bkup-raynaldmo-var-www.tgz
+
+# remove old backups if any
+if [ -f ${backup_loc}/${home_backup} ]
+then
+#    echo "Saving old backup ${backup_loc}/${home_backup}" 
+#    mv -f ${backup_loc}/${home_backup} ${backup_loc}/${home_backup}-$(date +%Y-%m-%d-%H-%M-%S).tar.gz
+
+    echo "Removing old backup ${backup_loc}/${home_backup}" 
+    rm -f ${backup_loc}/${home_backup}" 
+fi
+
+if [ -f ${backup_loc}/${var_www_backup} ]
+then
+#    echo "Saving old backup ${backup_loc}/${var_www_backup}" 
+#    mv -f ${backup_loc}/${var_www_backup} ${backup_loc}/${var_www_backup}-$(date +%Y-%m-%d-%H-%M-%S).tar.gz
+
+    echo "Removing old backup ${backup_loc}/${var_www_backup}" 
+    rm -f ${backup_loc}/${var_www_backup}" 
+fi
+
+cd ${backup_loc}
+
+echo "Backing up ${home_dir}..."
+tar czf ${backup_loc}/${home_backup} ${home_dir}
+
+echo "Backing up ${www_dir}..."
+tar czf ${backup_loc}/${var_www_backup} ${www_dir}
+
+echo "Done!"
